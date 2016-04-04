d39fd4a1dfe15fb9027aa041f55d65a3659e3551
sycx2 <dk891@gmx.de>
2016-04-04 12:18:37 +0200

    mainwindow implementation

---
+++ src/mainwindow.cpp
@@ -0,0 +1,57 @@
+#include "mainwindow.h"
+#include "MatrixModel.h"
+
+
+
+MainWindow::MainWindow() {
+    //textEdit = new QPlainTextEdit;
+    //setCentralWidget(textEdit);
+    tableView = new QTableView;	
+    dima::matrix<int> m = {{1,2,3},{4,5,6},{7,8,9}};
+    MatrixModel<int>* model = new MatrixModel<int>(m);
+    tableView->setModel(model);
+    setCentralWidget(tableView);
+
+    createActions();
+    createMenus();
+	//createToolBars();
+	//createStatusBar();
+	
+	//readSettings();
+	
+	//connect(
+
+}
+
+/*template<typename T>
+void MainWindow::displayMatrix(dima::matrix<T> m) {
+    MatrixModel<int> model(m);
+    this->tableView->setModel(&model);
+}*/
+
+void MainWindow::createActions() {
+    exitAct = new QAction(tr("Exit"), this);
+    exitAct->setShortcuts(QKeySequence::Quit);
+    exitAct->setStatusTip(tr("Exit the application"));
+    connect(exitAct, SIGNAL(triggered()), this, SLOT(close()));
+}
+
+void MainWindow::createMenus() {
+	fileMenu = menuBar()->addMenu(tr("&File"));
+    menuBar()->setNativeMenuBar(false);// Trick to see native MenuBar
+    //fileMenu->addAction(newAct);
+    //fileMenu->addAction(openAct);
+    //fileMenu->addAction(saveAct);
+    fileMenu->addSeparator();
+    fileMenu->addAction(exitAct);
+
+    //editMenu = menuBar()->addMenu(tr("&Edit"));
+    //editMenu->addAction(cutAct);
+    //editMenu->addAction(copyAct);
+
+    menuBar()->addSeparator();
+
+    helpMenu = menuBar()->addMenu(tr("&Help"));
+    //helpMenu->addAction(aboutAct);
+    //helpMenu->addAction(aboutQtAct);*/
+}
