fa11fd3eda6ea461715b31e61a8bdc61928cdcbe
sycx2 <dk891@gmx.de>
2016-04-06 12:52:05 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-#include "rational.h"
-#include "gcd_lcm.h"
+
+#include "mainwindow.h"
+#include <QApplication>
 int main(int argc, char* argv[]) {
-	dima::rational r(false, 2, 4);
-	std::cout << r;
-	
- return 0; 
+    QApplication app(argc, argv);
+    app.setOrganizationName("Muhtech");
+    app.setApplicationName("Wimat 2");
+    MainWindow mainWin;
+    mainWin.show();
+
+    return app.exec();
 }
