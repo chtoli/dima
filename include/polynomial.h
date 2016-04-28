dcc2f79a06c135192743ced47e6dfaf460cf911c
chtoli <ch.t.linz@googlemail.com>
2016-04-28 17:41:32 +0200

    added the first methods to polynomial

--- include/polynomial.h
+++ include/polynomial.h
@@ -1,1 +1,120 @@
- 
+ #ifndef POLYNOMIAL_H
+#define POLYNOMIAL_H
+
+#include <ostream>
+#include <vector>
+
+namespace dima {
+	//! This Class represents Polynomials over an commutative unitary ring 
+	/*!
+	* Internally an std::vector is used for storaging the leading coefficients. The first entry is the constant coefficient, the second the coefficient of x^1 and so on.
+	* A normalize method is used, s.t. the last entry is always nonzero (except for the zero polynomial)
+	*/
+	template <typename T>
+	class polynomial {
+	using size_t = typename std::vector<T>::size_type;
+	private:
+		std::vector<T> data;
+		//! Removes all zeros in data after the leading coefficient
+		void normalize();
+		char c = 'X';
+	public:
+		//! Constructs the Zero Polynomial
+		polynomial() { data.push_back(0); }
+		//! Constructs a Polynomial from a vector
+		/*!
+		* \param v the first entry of v represents the constant coefficient, the second the linear coefficient and so on.
+		*/ 
+		polynomial(const std::vector<T>& v) : data(v) { normalize(); }
+		
+		//! Comparison operator
+		/*!
+		* \param rhs right hand side of the comparison
+		* \return true if *this and rhs are equal (as polynomials), false otherwise
+		*/
+		bool operator==(const polynomial& rhs) const;
+		//! Comparison operator
+		/*!
+		* \param rhs right hand side of the comparison
+		* \return true if *this and rhs are unequal (as polynomials), false otherwise
+		*/
+		bool operator!=(const polynomial& rhs) const;
+		
+		//! Unary + operator. Returns this polynomial
+		/*!
+		* \return *this
+		*/ 
+		polynomial operator+() const;
+		//! Unary + operator. Returns the additive inverse 
+		/*!
+		* \return the additive inverse of *this
+		*/ 
+		polynomial operator-() const;
+		//! Prefix increment operator
+		polynomial& operator++();
+		//! Prefix decrement operator
+		polynomial& operator--();
+		//! Posfix increment operator
+		polynomial operator++(int);
+		//! Postfix decrement operator
+		polynomial operator--(int);
+		
+		//! Sets the displayed variable for this polynomial (default: X)
+		void setC(char);
+		
+		//! Outputstream operator
+		/*!
+		* Writes the given polynomial to the stream.
+		* \param os the stream 
+		* \param p the polynomial
+		* \return the modified stream
+		*/ 
+		template <typename S>
+		friend std::ostream& operator<< (std::ostream& os, const polynomial<S> p);
+		
+		//! degree function
+		/*!
+		 * \param p the polynomial
+		 * \return degree of p
+		 */ 
+		template <typename S>
+		friend int deg( const polynomial<S> p);
+	};
+
+	template <typename T>
+	std::ostream& operator<< (std::ostream& os, const polynomial<T> p) {
+		os << p.data[0] << p.c;
+		for (size_t i = 1; i < p.data.size(); i++) {
+			if (p.data[i] >= 0) os << '+';
+			os << p.data[i] << p.c << "^(" << i << ")";
+		}
+		return os;
+	}
+	
+	template <typename T>
+	int deg( const polynomial<T> p) { 
+		if (p.data.size() == 1 && p.data[0] == 0) return -1;
+		return  p.data.size()-1;
+	}
+	
+	template <typename T>
+	void polynomial<T>::normalize() {
+		for (size_t i = data.size(); i > 1; i++) {
+			if (data[i] != 0) break;
+			data.pop_back();
+		}
+	}
+	
+	template <typename T>
+	polynomial<T> polynomial<T>::operator+() const {
+		return *this;
+	}
+	
+	template <typename T>
+	polynomial<T> polynomial<T>::operator-() const {
+		std::vector<T> p(deg(*this)+1);
+		for (size_t i = 0; i < p.size(); i++) p[i] = -(this->data[i]);
+		return p;
+	}
+}
+#endif
